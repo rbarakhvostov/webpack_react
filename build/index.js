e42bd7e0db83ac82bf5220c05cd7d669fb4eadc0
rbarakhvostov <rbarakhvostov@gmail.com>
2020-01-01 21:50:57 +0300

    apply MiniCssExtractPlugin

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './main.css';
+import './main.scss';
 
 const App = () => React.createElement("p", null, "Hello");
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: "development",
@@ -40,12 +41,12 @@ module.exports = {
       //loading css
       {
         test: /\.(css)$/,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       //loading sass/scss
       {
         test: /\.(s[ca]ss)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
     ]
   },
@@ -54,6 +55,9 @@ module.exports = {
       title: 'Hello world',
       buildTime: new Date().toISOString(),
       template: 'public/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main-[hash:8].css'
     })
   ]
 }

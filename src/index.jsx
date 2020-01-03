584286bb5f05d4a447e7a6596b7daa62773a1fc3
rbarakhvostov <rbarakhvostov@gmail.com>
2020-01-03 17:12:09 +0300

    test husky with lint-staged

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import './main.scss';
 import url from './images/avatar.png';
 
 const App = () => (
-  <div>
+    <div>
     <p>This is Webpack React App</p>
     <img src={url} alt="avatar" />
   </div>
